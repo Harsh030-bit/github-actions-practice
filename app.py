9e1c9fda7b60ec43518e2680b935f88ca3000bb5
Harsh030-bit <85543914+Harsh030-bit@users.noreply.github.com>
2026-07-08 02:42:38 +0530

    Update app.py - Dlibrate Python mistake linter test

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #the code is taken from one of remote repo
 # Doing test 3 for linter
+#Doing a Deliberate syntax error in code to check the linter working through ZGithub Action 
 
+import os  # Deliberate mistake: unused import, Ruff should catch this
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -8,6 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+Adding a secuity check {
+    def Sec Harshit_error
 
 
 @app.route('/health')
